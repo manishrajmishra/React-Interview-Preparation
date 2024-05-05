1bc811fcccae3fd7fabed3dd33c1fcc27c981543
manishrajmishra <rajmanish6851@gmail.com>
2024-05-05 14:09:17 +0530

    Basic Of Reacts

---
+++ 01basicreactconcepts/src/Youtube.js
@@ -0,0 +1,7 @@
+function Youtube() {
+    return (
+        <h1>Youtbe Create</h1>
+    )
+}
+
+export default Youtube
---
+++ 01basicreactconcetsvite/src/App.jsx
@@ -0,0 +1,11 @@
+import Youtube from "./Youtube";
+function App() {
+  return (
+    <>
+      <h1>Vite App</h1>
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
